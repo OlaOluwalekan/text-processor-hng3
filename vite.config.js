3ab3caaef5e7bada1c28fde88a23ba50429ecb09
OlaOluwalekan <olalekanbello534@gmail.com>
2025-02-18 17:03:34 +0100

    chore: update project configuration and HTML for environment variables

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_TRANSLATOR_TOKEN': JSON.stringify(
+      process.env.VITE_TRANSLATOR_TOKEN
+    ),
+    'process.env.VITE_DETECTOR_TOKEN': JSON.stringify(
+      process.env.VITE_DETECTOR_TOKEN
+    ),
+    'process.env.VITE_SUMMARIZER_TOKEN': JSON.stringify(
+      process.env.VITE_SUMMARIZER_TOKEN
+    ),
+  },
 })
